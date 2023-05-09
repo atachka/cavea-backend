da9d6e5669b339c543b3f06509d865d3c88adae5
atachka <56038697+atachka@users.noreply.github.com>
2023-05-09 16:26:01 +0400

    fixed api bug

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use(cors({ origin: ['http://localhost:3000'] }));
 
-app.use('/api/inventory', inventoryRouter);
+app.use('/api/inventories', inventoryRouter);
 
 app.use(globalErrorHandler);
 
